33335aafbec5772dc07cb14bb409146b075fab09
zj837578609 <35033294+zj837578609@users.noreply.github.com>
2019-05-07 17:00:08 +0800

    Add files via upload

---
+++ 1_15.py
@@ -0,0 +1,33 @@
+# #day1
+# import turtle
+#
+# turtle.pensize(4)
+# turtle.pencolor('red')
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.mainloop()
+
+
+#day2
+# #day1
+# import turtle
+#
+# turtle.pensize(4)
+# turtle.pencolor('red')
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.mainloop()
+
+
+#day2
+
